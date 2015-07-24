dd6911d13014293c4bf53f54092400425840b459
Kenneth J. Pronovici <pronovic@ieee.org>
2015-07-24 14:58:41 -0500

    Remove obsolete utilities

--- util/svnupdate.bat
+++
@@ -1,4 +0,0 @@
-REM Simple script to grab SVN trunk when Subversion protocol isn't allowed through proxy
-REM Set http_proxy to the proxy address, and then place the user/password into the command-line.
-set http_proxy=proxy.XXXXXX.com:XXXX
-wget --proxy=on --proxy-user=XXXXXX --proxy-passwd=XXXXXXXX -e robots=off -r -np -nH -R index.html -P cedar-backup2 --cut-dirs=4 -I /svn/software/cedar-backup2/trunk -X /svn/software/cedar-backup2/tags http://cedar-solutions.com/svn/software/cedar-backup2/trunk
