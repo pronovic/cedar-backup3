368f85e5f09afa615c1e74d82b0b3acd29249a61
Kenneth J. Pronovici <pronovic@ieee.org>
2025-09-17 20:49:14 -0500

    Address PLC1901

--- src/CedarBackup3/util.py
+++ src/CedarBackup3/util.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
+# ruff: noqa: PLC1901
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #              C E D A R
